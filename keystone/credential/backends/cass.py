5cd094e5a99687c0ecb9ba9ae01b00e8b87051bd
Rushi Agrawal <rushi.agr@gmail.com>
2015-06-05 12:56:24 +0000

    Remove stray pdb statement

--- keystone/credential/backends/cass.py
+++ keystone/credential/backends/cass.py
@@ -11,7 +11,7 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-import pdb; pdb.set_trace()
+
 from keystone.common import cass
 from keystone import credential
 from keystone import exception
